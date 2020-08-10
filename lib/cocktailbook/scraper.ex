cc67f66cf4363bdb30d999c42546aaa9c92bdb39
Rui Freitas <dinnerpartysaboteur@gmail.com>
2020-08-10 11:39:25 +0200

    Scrape individual Cocktail pages

--- lib/cocktailbook/scraper.ex
+++ lib/cocktailbook/scraper.ex
@@ -1,12 +1,14 @@
 defmodule Cocktailbook.Scraper do
   @moduledoc false
 
-  @base_url "https://www.bbcgoodfood.com/search"
+  @base_url "https://www.bbcgoodfood.com"
+
+  alias Cocktailbook.Repo.Cocktail
 
   def fetch_cocktails(query, count \\ 5) do
     HTTPoison.start
     response =
-      @base_url <> "/recipes?query=#{query}"
+      @base_url <> "/search/recipes?query=#{query}"
       |> HTTPoison.get
     case response do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
@@ -18,15 +20,53 @@ defmodule Cocktailbook.Scraper do
     end
   end
 
-  def fetch_cocktail(url \\ "") do
+  def fetch_cocktail(path \\ "/recipes/sherry-negroni") do
+    HTTPoison.start
+    response =
+      @base_url <> path
+      |> HTTPoison.get
+    case response do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        parse_cocktail(body)
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        IO.puts "Not found :("
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.inspect reason
+    end
+  end
+
+  defp parse_cocktail(body) do
+    %Cocktail{}
+    |> parse_cocktail_name(body)
+    |> parse_cocktail_ingredients(body)
+  end
+
+  defp parse_cocktail_name(%Cocktail{} = cocktail, body) do
+    name =
+      body
+      |> Floki.parse_document!
+      |> Floki.find("h1[itemprop=\"name\"]")
+      |> Floki.text
+      |> String.trim
+    %{ cocktail | name: name }
+  end
+
+  defp parse_cocktail_ingredients(%Cocktail{} = cocktail, body) do
+    ingredients =
+      body
+      |> Floki.parse_document!
+      |> Floki.find(".ingredients-list ul > li")
+      |> Enum.map(&Floki.attribute(&1, "content"))
+      |> Enum.join(", ")
+    %{ cocktail | ingredients: ingredients }
   end
 
   defp parse_recipes(body, count) do
     body
+    |> Floki.parse_document!
     |> Floki.find(".view-content article")
     |> Enum.take(count)
     |> Enum.map(&parse_article(&1))
-    |> IO.inspect
   end
 
   defp parse_article(article) do
--- test/scraper_test.exs
+++ test/scraper_test.exs
@@ -2,12 +2,13 @@ defmodule ScraperTest do
   use ExUnit.Case, async: true
 
   alias Cocktailbook.Scraper
+  alias Cocktailbook.Repo.Cocktail
 
   describe "#fetch_cocktails" do
     test "returns a list of 5 scraped cocktails" do
       result = Scraper.fetch_cocktails("mojito")
 
-      assert result = [
+      assert result == [
         %{
           href: "/recipes/mojito-pitcher",
           name: "Mojito pitcher"
@@ -33,7 +34,7 @@ defmodule ScraperTest do
     test "accepts an optional number of cocktails to scrape argument" do
       result = Scraper.fetch_cocktails("negroni", 2)
 
-      assert result = [
+      assert result == [
         %{
           href: "/recipes/classic-negroni",
           name: "Classic negroni"
@@ -43,6 +44,17 @@ defmodule ScraperTest do
           name: "Sherry negroni"
         }
       ]
+    end
+  end
+
+  describe "#fetch_cocktail" do
+    test "returns aCocktail struct with scraped data" do
+      result = Scraper.fetch_cocktail("/recipes/classic-negroni")
+
+      assert result == %Cocktail{
+        name: "Classic negroni",
+        ingredients: "25ml gin, 25ml sweet vermouth (we used Antica formula), 25ml Campari, ice, slice of orange"
+      }
     end
   end
 end
